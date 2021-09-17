7ecb59489bc341989cd0b576deed43af1e9b8680
ignoreandfly <77511196+ignoreandfly@users.noreply.github.com>
2021-09-18 01:29:11 +0530

    feat: chalk npm added

--- markone_levelzero.js
+++ markone_levelzero.js
@@ -1,13 +1,15 @@
 var readlineSync = require("readline-sync");
 var name = readlineSync.question("Please enter your name ");
 var score = 0;
-console.log("Welcome to HOW WELL DO YOU KNOW SIDDHANT, "+ name + ".")
+const chalk = require('chalk');
+
+console.log(chalk.blue("Welcome to HOW WELL DO YOU KNOW SIDDHANT, "+ name + "."))
 function play(question, answer){
   var questionNow = readlineSync.question(question)
   if (questionNow === answer) {
-    console.log("You're right! "); score = score+1
+    console.log(chalk.green("You're right! ")); score = score+1
    }else{
-     console.log("You're wrong :( "); score = score-1
+     console.log((chalk.red("You're wrong :( "))); score = score-1
    }
   }
 var currentQuestion =[{question:"Who is Siddhant's favourite philosopher ", answer: "Nietzsche"},{question:"Which social media site is Siddhant most active on? ", answer: "Twitter"}, {question:"What is the name of Siddhant's favourite band? ", answer: "Coldplay"},{question:"What kind of book does Siddhant love reading? ",answer: "Non-fiction"},{question:"Who is Siddhant's favoutite filmmaker? ", answer: "Wes Anderson"}];
@@ -16,4 +18,4 @@ for (i=0; i<currentQuestion.length; i++){
   play (questionBank.question, questionBank.answer)
 }
 console.log("-------------");
-console.log("Your final score is: " + score);
+console.log(chalk.purple("Your final score is: " + score));
